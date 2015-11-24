2ba088aea215fd84996b0a46266cbdb06cbdd1ea
Josh Parks <josh.parks@nike.com>
2015-11-23 17:01:13 -0800

    Adding initial grunt configurations

---
+++ Gruntfile.js
@@ -0,0 +1,31 @@
+module.exports = function(grunt) {
+
+  // Project configuration.
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    uglify: {
+      options: {
+        banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
+      },
+      build: {
+        src: 'src/<%= pkg.name %>.js',
+        dest: 'build/<%= pkg.name %>.min.js'
+      }
+    },
+    jshint: {
+      options: {
+        jshintrc: '.jshintrc'
+      },
+      all: ['Gruntfile.js', 'app/**/*.js', '!app/node_modules/*.js', '!app/bower_modules/*.js']
+    },
+  });
+
+  // Load the plugin that provides the "uglify" task.
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-sass');
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+
+  // Default task(s).
+  grunt.registerTask('default', ['uglify']);
+
+};
