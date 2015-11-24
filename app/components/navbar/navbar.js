4981bd0c9828f713afde70b7e5eb7deb30c2d313
Josh Parks <josh.parks@nike.com>
2015-11-24 11:24:01 -0800

    Adding navigational functionality.

--- app/components/navbar/navbar.js
+++ app/components/navbar/navbar.js
@@ -7,9 +7,13 @@ angular.module('experiments.navbar', [
 .controller('NavBarCtrl', ['$scope', function($scope) {
 
   $scope.tabs = [
-    { title:'Home Title', content:'Home Content', url: '#/home' },
-    { title:'View1 Title', content:'View 1 Content', url: '#/view1'},
-    { title:'View2 Title', content:'View 2 Content', url: '#/view2'}
+    { title:'Home', href: '#/home' },
+    { title:'View 1', href: '#/view1'},
+    { title:'View 2', href: '#/view2'}
   ];
+
+  $scope.changeHash = function(data) {
+    window.location.hash = data;
+  };
 
 }]);
